66b21a7843000dd0eb73d93229b852dbd79ab9ec
Tagdanai Sujinnapram <hummoosr17@gmail.com>
2017-06-13 15:09:03 +0700

    test

---
+++ src/main/java/com/example/demo/LineBackendApplication.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.demo.service.TableService;
+
+@SpringBootApplication
+public class LineBackendApplication implements CommandLineRunner {
+
+	@Autowired
+	TableService tableService;
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(LineBackendApplication.class, args);
+
+	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		tableService.dropTable();
+		tableService.createLineGroup();
+	}
+
+}
